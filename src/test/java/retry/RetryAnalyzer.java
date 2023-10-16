72288eef3a098c81259736330a1a79e933114190
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-16 19:03:34 +0200

    Add RetryAnalyzer to repeat the test several times

---
+++ src/test/java/retry/RetryAnalyzer.java
@@ -0,0 +1,16 @@
+package retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer{
+    private int count = 0;
+    private int maxRetry = 2;
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry){
+            return true;
+        }
+        return false;
+    }
+}
