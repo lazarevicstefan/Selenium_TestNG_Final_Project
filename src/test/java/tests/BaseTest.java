0329aed8324565a4cf8eee0fee2a6c2aa8244040
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-17 21:05:55 +0200

    Add Abstract BaseTest class with all setup

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,68 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "";
+    protected LoginPage loginPage;
+    protected NavPage navPage;
+    protected SignupPage signupPage;
+    protected CitiesPage citiesPage;
+    protected MessagePopUpPage messagePopUpPage;
+    @BeforeClass
+    public void setup (){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver, wait);
+        navPage = new NavPage(driver, wait);
+        signupPage = new SignupPage(driver, wait);
+        citiesPage = new CitiesPage(driver, wait);
+        messagePopUpPage = new MessagePopUpPage(driver, wait);
+    }
+    @BeforeMethod
+    public void navigate (){
+        driver.navigate().to(baseUrl);
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void screenshotIfTestFail (ITestResult result) throws IOException {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss");
+        String stringDate = dateFormat.format(date);
+
+        if (ITestResult.FAILURE==result.getStatus()) {
+            File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            Files.copy(file.toPath(), new File("screenshots/screenshot-" + stringDate + ".jpg").toPath());
+        }
+    }
+    @AfterClass
+    public void exit(){
+        driver.quit();
+    }
+}
