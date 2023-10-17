9b2b523daa945c64bad373399e7054f46c0ba108
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-17 22:04:02 +0200

    Update LoginTests with test checksInputTypes

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -13,4 +13,18 @@ public class LoginTests extends BasicTest {
         Assert.assertTrue(loginPage.verifyIsLoginPage()
                 , "Actual URL doesn't match expected URL");
     }
+    @Test(priority = 2,retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypes(){
+        String expectedEmail = "email";
+        String expectedPassword = "password";
+
+        navPage.clickOnLoginButton();
+        String actualEmail = loginPage.getEmailElement().getAttribute("type");
+        String actualPassword = loginPage.getPasswordElement().getAttribute("type");
+
+        Assert.assertEquals(actualEmail, expectedEmail
+                ,"Actual attribute value doesn't match expected attribute value");
+        Assert.assertEquals(actualPassword, expectedPassword
+                ,"Actual attribute value doesn't match expected attribute value");
+    }
 }
