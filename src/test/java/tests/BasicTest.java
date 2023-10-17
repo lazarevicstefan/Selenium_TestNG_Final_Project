10c120cb8b87a10688cf88076e6792664ece06ed
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-17 21:07:43 +0200

    Update title name

--- src/test/java/tests/BasicTest.java
+++ src/test/java/tests/BasicTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -23,7 +22,7 @@ import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
 
-public abstract class BaseTest {
+public abstract class BasicTest {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected String baseUrl = "";
