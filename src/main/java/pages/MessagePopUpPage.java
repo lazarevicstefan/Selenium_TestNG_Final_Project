f9fcbff871a5545b1ad1c77298f9ead3dfea8177
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-17 19:28:58 +0200

    Add MessagePopUpPage tests setup

---
+++ src/main/java/pages/MessagePopUpPage.java
@@ -0,0 +1,64 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MessagePopUpPage extends BasicPage {
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void waitForPopUpDialogToVisible() {
+        wait.until(ExpectedConditions.attributeContains(By
+                .xpath("//*[contains(@class, 'background ')]"), "class", "v-snack--active"));
+    }
+
+    public String getPopUpMessageString() {
+        return wait.until(ExpectedConditions
+                        .visibilityOfElementLocated(By
+                                .xpath("//*[contains(@class, 'background ')]//li")))
+                                        .getText();
+    }
+
+    public WebElement getPopUpMessageElement() {
+        return wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By
+                        .xpath("//*[contains(@class, 'background ')]//li")));
+    }
+    public void clickOnCloseButton (){
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By
+                        .xpath("//*[contains(@class, 'v-snack__content')]/button")))
+                                .click();
+    }
+    public WebElement getCloseButtonElement (){
+        return wait.until(ExpectedConditions
+                        .visibilityOfElementLocated(By
+                                .xpath("//*[contains(@class, 'v-snack__content')]/button")));
+    }
+    public void waitForVerifyYourAccountToBeVisible() {
+        wait.until(ExpectedConditions
+                .visibilityOfAllElementsLocatedBy(By
+                        .className("dlgVerifyAccount")));
+    }
+    public String getTextFromVerifyYourAccount() {
+        return wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By
+                        .xpath("//*[contains(@class, 'v-card__text') and contains(@class, 'mt-4')]")))
+                                .getText();
+    }
+    public void clickOnCloseButtonFromVerifyYourAccount(){
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By
+                        .xpath("//*[contains(@class, 'v-card__actions')]/button")))
+                                .click();
+    }
+    public WebElement getCloseButtonFromVerifyYourAccountElement(){
+        return wait.until(ExpectedConditions
+                        .visibilityOfElementLocated(By
+                                .xpath("//*[contains(@class, 'v-card__actions')]/button")));
+    }
+}
