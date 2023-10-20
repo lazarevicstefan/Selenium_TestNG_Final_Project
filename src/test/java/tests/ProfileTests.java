ef41a8b23766a1644218c32fbfd3a6695e3f0fb3
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-20 18:36:03 +0200

    Update ProfileTests with test editsProfile

--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -25,7 +25,7 @@ public class ProfileTests extends BasicTest{
 
         navPage.clickOnLogOutButton();
     }
-    @Test (priority = 2)
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
     public void checksInputTypes() {
         String expectedEmail = "email";
         String expectedEmailDisable = "disabled";
@@ -64,6 +64,55 @@ public class ProfileTests extends BasicTest{
         Assert.assertEquals(profilePage.getProfileGitHubElement().getAttribute("type"), expectedUrlGitHub
                 , "Actual attribute value doesn't match expected attribute value");
         Assert.assertEquals(profilePage.getProfilePhoneElement().getAttribute("type"), expectedPhone
+                , "Actual attribute value doesn't match expected attribute value");
+
+        navPage.clickOnLogOutButton();
+    }
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void editsProfile () {
+        String email = "admin@admin.com";
+        String password = "12345";
+        String fullName = "Stefan Lazarevic";
+        String phone = "+381612345678";
+        String city = "Bucaramanga";
+        String country = "Spain";
+        String twitter = "https://twitter.com/profile";
+        String github = "https://github.com/lazarevicstefan";
+        String expectedErrorMessage = "Profile saved successfuly\nCLOSE";
+
+        navPage.clickOnLoginButton();
+
+        loginPage.clearAndEnterEmail(email);
+        loginPage.clearAndEnterPassword(password);
+        loginPage.clickOnLoginButton();
+
+        navPage.clickOnMyProfilePageButton();
+
+        profilePage.clearAndEnterProfileName(fullName);
+        profilePage.clearAndEnterProfilePhone(phone);
+        profilePage.clearAndEnterProfileCity(city);
+        profilePage.clearAndEnterProfileCountry(country);
+        profilePage.clearAndEnterProfileTwitter(twitter);
+        profilePage.clearAndEnterProfileGitHub(github);
+        profilePage.clickOnSaveButton();
+
+        messagePopUpPage.waitForPopUpSuccessfullyMessageToShow();
+        Assert.assertEquals(messagePopUpPage.getSuccessfullyMessageString(),expectedErrorMessage
+                ,"");
+
+        Assert.assertEquals(profilePage.getProfileEmailString(), email
+                , "Actual field value doesn't match expected field value");
+        Assert.assertEquals(profilePage.getProfileNameString(), fullName
+                , "Actual field value doesn't match expected field value");
+        Assert.assertEquals(profilePage.getProfilePhoneString(), phone
+                , "Actual field value doesn't match expected field value");
+        Assert.assertEquals(profilePage.getProfileCityString(), city
+                , "Actual field value doesn't match expected field value");
+        Assert.assertEquals(profilePage.getProfileCountryString(), country
+                , "Actual field value doesn't match expected field value");
+        Assert.assertEquals(profilePage.getProfileTwitterString(), twitter
+                , "Actual attribute value doesn't match expected attribute value");
+        Assert.assertEquals(profilePage.getProfileGitHubString(), github
                 , "Actual attribute value doesn't match expected attribute value");
 
         navPage.clickOnLogOutButton();
